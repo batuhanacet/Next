124c934bea75cf6f6401f9b7287bd7b838af1553
batuhanacet <batuhan.acet@metu.edu.tr>
2021-05-21 19:07:57 +0300

    batuhan

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,19 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost(){
-    return <>
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">
-                <a>Back to Home</a>
-            </Link>
-        </h2>
-        <Image 
-            src="/images/profile.jpg"
-            height={144}
-            width={144}
-            alt="foto"
-        />
-    </>
+    return (
+    <Layout>
+        main part
+    </Layout>
+    )
+        
+    
 } 
